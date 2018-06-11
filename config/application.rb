06eb521e340147aac7d6bc1aa85175f791c26e81
Damian Galarza <damian@thoughtbot.com>
2018-06-11 18:32:56 -0400

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+Bundler.require(*Rails.groups)
+module Pdf2text
+  class Application < Rails::Application
+    config.assets.quiet = true
+    config.generators do |generate|
+      generate.helper false
+      generate.javascripts false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+    config.action_controller.action_on_unpermitted_parameters = :raise
+    config.load_defaults 5.2
+    config.generators.system_tests = nil
+  end
+end
